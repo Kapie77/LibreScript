ab70b648dd7c773e16c6e6726340c9f7e71019b3
Kapie77 <usergames02@proton.me>
2026-08-15 22:15:04 -0300

    Add files via upload

---
+++ src/data/sampleScript.ts
@@ -0,0 +1,75 @@
+import type { ScriptBlock } from "../types/script";
+
+export const sampleScript: ScriptBlock[] = [
+  {
+    id: 1,
+    type: "scene",
+    content: "EXT. - EMPIRE STATE BUILDING - DAY",
+  },
+  {
+    id: 2,
+    type: "action",
+    content: "Clark Kent pulls up in a cab. The door opens, and he steps out onto the curb as Lex Luther walks out the front door of the building.",
+  },
+  {
+    id: 3,
+    type: "character",
+    content: "CLARK",
+  },
+  {
+    id: 4,
+    type: "dialogue",
+    content: "Hello Mr. Luther, I've been wanting to buy you a cup of coffee. Do you have a few minutes to spare?",
+  },
+
+  {
+    id: 5,
+    type: "character",
+    content: "LEX",
+  },
+  {
+    id: 6,
+    type: "dialogue",
+    content: "You think I don't know who you are?",
+  },
+
+  {
+    id: 7,
+    type: "character",
+    content: "CLARK",
+  },
+  {
+    id: 8,
+    type: "dialogue",
+    content: "Who I am? Sir, I am a mild-mannered reporter at the Daily Record.",
+  },
+
+  {
+    id: 9,
+    type: "character",
+    content: "LEX",
+  },
+  {
+    id: 10,
+    type: "parenthetical",
+    content: "(Wryly)",
+  },
+  {
+    id: 11,
+    type: "dialogue",
+    content: "But what if I did this?",
+  },
+
+  {
+    id: 12,
+    type: "action",
+    content: "Lex pulls a laser out from under his topcoat and cuts off the top of the Empire State Building.",
+  },
+
+   {
+    id: 13,
+    type: "transition",
+    content: "CUT TO:",
+  },
+
+];
